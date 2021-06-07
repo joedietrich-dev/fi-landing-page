45d4dcef208e9922f412b1e35d7df490547c0c08
Joe Dietrich <joe@joedietrich.net>
2021-06-06 22:11:02 -0400

    Removed Placeholder text, allow theme persistence

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # fi-landing-page
+
+To Do:
+1. Break Projects into separate page
+2. Create separate page for Array functions article
+3. Write copy for links to Projects and Arrays
--- script.js
+++ script.js
@@ -2,19 +2,34 @@ const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript"; 
 document.querySelector('main').appendChild(h2); 
 
+function themeStore(value) {
+  
+}
+
 function main() {
   const body = document.querySelector('body');
   const themeToggler = document.getElementById('theme-switch');
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    body.classList.add('dark');
+  if (!localStorage.getItem('theme')) {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      body.classList.add('dark');
+    } else {
+      body.classList.add('light');
+    }    
   } else {
-    body.classList.add('light');
+    if (localStorage.getItem('theme') === 'dark') {
+      body.classList.add('dark');
+    } else {
+      body.classList.add('light');
+    }    
   }
+
   themeToggler.addEventListener('click', () => {
     if (body.classList.contains('dark')) {
       body.classList.replace('dark', 'light');
+      localStorage.setItem('theme', 'light');
     } else {
-      body.classList.replace('light', 'dark')
+      body.classList.replace('light', 'dark');
+      localStorage.setItem('theme', 'dark');
     }
   })
 }
